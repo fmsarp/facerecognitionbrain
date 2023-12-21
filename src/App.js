d867148180463b9f1ec6cdf759de0dc72001f619
fmsarp <fmsarp@gmail.com>
2023-12-21 06:36:08 -0500

    Added tsparticles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
-import './App.css';
+import Rank from './components/Rank/Rank';
+import Particles from './components/Particles/Particles';
+import "./App.css";
+
 
 export default function App() {
   return (
     <div className="App">
+      <Particles className="particles"/>
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
             {/* {<FaceRecognition /> } */}
     </div>
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,12 @@
+export default function Rank() {
+  return (
+    <div>
+      <div className="white f3">
+        {'Franklin, you current rank is...'}
+      </div>
+      <div className="white f1">
+        {'#5'}
+      </div>
+    </div>
+  );
+}
